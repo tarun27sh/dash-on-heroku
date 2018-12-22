8b6b838c817d635c695d897a3560aab4b7273f0f
Tarun Sharma <tarusha2@gmail.com>
2018-12-21 22:31:00 -0800

    scapy re-order import

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ import plotly.graph_objs as go
 import psutil as ps
 import threading
 import time
-from platform import platform
 from scapy.all import *
+from platform import platform
 
 
 class Data:
